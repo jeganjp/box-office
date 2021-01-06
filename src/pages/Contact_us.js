d677d25c94f8364f52658cbde41ea8cbc2b93f03
jeganjp <jeganjp1999@gmail.com>
2021-01-06 19:39:40 +0530

    added radio buttons and actor searching options

---
+++ src/pages/Contact_us.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Contact_us = () => {
+    return (
+        <div>
+            This is contact-us page
+        </div>
+    )
+}
+
+export default Contact_us
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,8 @@ import {get_api} from '../misc/config';
 const Home = () => {
     const [input,setInput]=useState('');
     const [result,setResult]=useState(null);
+    const [searchOption,setSearchOption]=useState('shows');
+    const searchcheck = searchOption==='shows';
 
     const onInputChange=(ev)=>{
         //console.log(ev.target.value);
@@ -20,7 +22,13 @@ const Home = () => {
 
     const onSearch=()=>
     {
-        get_api(`search/shows?q=${input}`).then(result=>{setResult(result);console.log(result)});
+        get_api(`search/${searchOption}?q=${input}`).then(result=>{setResult(result);console.log(result)});
+    }
+
+    const radioButtonChange=(ev)=>
+    {
+        setSearchOption(ev.target.value);
+        console.log(ev.target.value);
     }
 
     const renderResults=()=>
@@ -31,16 +39,30 @@ const Home = () => {
         }
         if(result && result.length>0)
         {
-          return <div>{result.map((item)=><div key={item.show.id}>{item.show.name}
-          {/* <div><img src={item.show.image.medium} alt="show poster"/></div> */}
-          </div>)}</div>
+           return result[0].show?result.map((item)=>(<div key={item.show.id}>{item.show.name}</div>)):result.map((item)=>(<div key={item.person.id}>{item.person.name}</div>))
         }
+        // if(result && result.length>0 && searchOption==='people')
+        // {
+        //   return <div>{result.map((item)=><div key={item.person.id}>{item.person.name}
+        //   {/* <div style={{marginRight:10}}><img src={item.person.image.medium} alt='show poster'/></div> */}
+        //   </div>)}</div>
+        // }
         return null;
     }
 
     return (
         <MainPageLayout>
-            <input type="text" onChange={onInputChange} onKeyDown={onDownPress} value={input}/>
+            <input type="text" placeholder="Search for something" onChange={onInputChange} onKeyDown={onDownPress} value={input}/>
+            <div>
+                <label htmlFor="show-search">
+                    shows
+                    <input id="show-search" type="radio" value='shows' checked={searchcheck} onChange={radioButtonChange} />
+                </label>
+                <label htmlFor="actors-search">
+                    actors
+                    <input id="actors-search" type='radio' value='people' checked={!searchcheck} onChange={radioButtonChange} />
+                </label>
+            </div>
             <button type='button' onClick={onSearch}>Search</button>
             {renderResults()}
         </MainPageLayout>
