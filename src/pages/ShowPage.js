16e2785d942e0c11d021cd5f975e4eebb746b77b
jeganjp <jeganjp1999@gmail.com>
2021-01-11 19:51:38 +0530

    added loading and error msg in showPage

--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -3,16 +3,37 @@ import {useParams} from 'react-router-dom';
 import {get_api} from '../misc/config';
 const ShowPage = () => {
     const {id} =useParams();
-
     const [show,setShow]= useState(null);
+    const [isLoading,setIsLoading]=useState(true);
+    const [error,setError]=useState(null);
+
 
     useEffect(()=>{
+        let isMounted =true;
         get_api(`shows/1?embed[]=seasons&embed[]=cast`).then(results =>
         {
-            setShow(results);
+            if(isMounted){
+                setShow(results);
+                setIsLoading(false);
+            }
+         }).catch(err=>{
+             if(isMounted){
+                setError(err.message);
+                setIsLoading(false);
+             }
+            
         })
+        return ()=>{isMounted = false}
     },[id]);
     console.log('show',show);
+    if(isLoading)
+    {
+        return (<div>Page is loading</div>);
+    }
+    if(error)
+    {
+        return (<div>Error is : {error}</div>)
+    }
     return (
         <div>
             this is show pages
