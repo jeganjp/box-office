a4cd0e65dd05596788dba10c5736af1020243cde
jeganjp <jeganjp1999@gmail.com>
2021-01-17 13:35:29 +0530

    little modified in styling

--- src/components/shows/ShowSeasons.js
+++ src/components/shows/ShowSeasons.js
@@ -7,12 +7,12 @@ const ShowSeasons = ({seasons}) => {
             <div><p>Total Seasons: {seasons.length}</p></div>
             <SeasonList>{seasons.map((season)=>(
                 <div key={season.id} className="season-item">
-                    <p className="left">Season : {season.number}</p>
-                    <p>
-        Episodes in total:{' '}
-        <span>
+                    <p className="left">Season : {season.number}
+        <p>Episodes in total:{' '}
+        <span> 
           {seasons.reduce((acc, season) => acc + season.episodeOrder, 0)}
         </span>
+        </p>
       </p>
                     <p className="right">Aired : {season.premiereDate} - {season.endDate} </p>
                 </div>
