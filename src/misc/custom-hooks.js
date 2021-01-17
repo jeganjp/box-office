ca487469a7de375b9703a0292ffb998d89607b67
jeganjp <jeganjp1999@gmail.com>
2021-01-17 18:06:31 +0530

    2 new custom hooks useLastQuery and useShow created

--- src/misc/custom-hooks.js
+++ src/misc/custom-hooks.js
@@ -1,4 +1,5 @@
-import {useReducer,useEffect} from 'react';
+import {useReducer,useEffect,useState} from 'react';
+import { get_api } from './config';
 
 function showReducer(pervState,action)
 {
@@ -34,4 +35,56 @@ function usePersistantReducer(reducer,initialState,key)
 export function useShows(key='shows')
 {
     return usePersistantReducer(showReducer,[],key)
+}
+
+export function useLastQuery(key='lastQuery')
+{
+    const [input,setInput]= useState(()=>{
+        const persisted = sessionStorage.getItem(key);
+        return persisted?JSON.parse(persisted):"";
+    });
+    const setPersistedInput=(newState)=>{
+        setInput(newState);
+        sessionStorage.setItem(key,JSON.stringify(newState))
+    };
+    return [input,setPersistedInput]
+}
+
+const reducer=(prevState,action)=>{
+    switch(action.type)
+    {
+        case 'FETCH_SUCCESS':
+            return {show:action.show,isLoading:false,error:null}
+        case 'FETCH_FAILED':
+            return {...prevState,isLoading:false,error:action.error}
+        default: return prevState
+        }
+    
+}
+
+
+export function useShow(showId)
+{
+    const [state,dispatch] =useReducer(reducer,
+        {show:null,
+        isLoading:true,
+        error:null,
+        });  
+
+    useEffect(()=>{
+        let isMounted =true;
+        get_api(`shows/${showId}?embed[]=seasons&embed[]=cast`).then(results =>
+        {
+            if(isMounted){
+                dispatch({type:'FETCH_SUCCESS',show:results})
+            }
+         }).catch(err=>{
+             if(isMounted){
+                dispatch({type:'FETCH_FAILED', error:err.message})
+             }
+            
+        })
+        return ()=>{isMounted = false}
+    },[showId]);
+    return state;
 }
--- src/pages/ShowPage.js
+++ src/pages/ShowPage.js
@@ -1,6 +1,6 @@
-import React,{useEffect,useReducer} from 'react';
+import React from 'react';
 import {useParams} from 'react-router-dom';
-import {get_api} from '../misc/config';
+// import {get_api} from '../misc/config';
 // import NOT_FOUND_IMG from '../images/not-found.png';
 import ShowMainData from '../components/shows/ShowMainData';
 import ShowDetails from '../components/shows/ShowDetails';
@@ -8,45 +8,15 @@ import ShowSeasons from '../components/shows/ShowSeasons';
 import ShowCast from '../components/shows/ShowCast';
 import {ShowPageWrapper} from './Show.styled';
 import { InfoBlock } from './Show.styled';
+import { useShow } from '../misc/custom-hooks';
 const ShowPage = () => {
     const {id} =useParams();
     // const [show,setShow]= useState(null);
     // const [isLoading,setIsLoading]=useState(true);
     // const [error,setError]=useState(null);
 
-    const initialState ={
-        show:null,isLoading:true,error:null,
-    }
-
-    const reducer=(prevState,action)=>{
-        switch(action.type)
-        {
-            case 'FETCH_SUCCESS':
-                return {show:action.show,isLoading:false,error:null}
-            case 'FETCH_FAILED':
-                return {...prevState,isLoading:false,error:action.error}
-            default: return prevState
-            }
-        
-    }
-
-    const [{show,isLoading,error},dispatch] =useReducer(reducer,initialState);  
-
-    useEffect(()=>{
-        let isMounted =true;
-        get_api(`shows/${id}?embed[]=seasons&embed[]=cast`).then(results =>
-        {
-            if(isMounted){
-                dispatch({type:'FETCH_SUCCESS',show:results})
-            }
-         }).catch(err=>{
-             if(isMounted){
-                dispatch({type:'FETCH_FAILED', error:err.message})
-             }
-            
-        })
-        return ()=>{isMounted = false}
-    },[id]);
+    const {show,isLoading,error}=useShow(id);
+    
     console.log('show',show);
     if(isLoading)
     {
